febb3df5ce1c5ee3e1b9cb8bb80319756f060088
Mohsin231 <48874512+Mohsin231@users.noreply.github.com>
2019-11-04 19:48:40 -0500

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+//print numbers 0-100
+//If the number is divisible by 3, print "Fizz"
+//divisible by 5, print "Buzz"
+// if divisible by both, print "FizzBuzz"
+
+public class FizzBuzz{
+  public static void main(String[] args){
+  for (int i=0; i<=100; i++) {
+    if (i%3==0&&i%5==0) {
+      System.out.println("FizzBuzz");
+    }
+    else if (i%3==0){System.out.println("Fizz");}
+    else if (i%5==0){System.out.println("Buzz");}
+    else {System.out.println(i);}
+  }
+}
+}
